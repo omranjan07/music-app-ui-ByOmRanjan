751efa0f3f30df81755cd89fd517168c33349597
Om <omranjankvrajgir@gmail.com>
2025-05-15 13:55:07 +0530

    Initial commit - adding project files

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useRoutes } from "react-router";
+import routes from "./routes";
+
+import './App.css'
+
+function App() {
+	const element = useRoutes(routes);
+	return <>{element}</>;
+}
+
+export default App;
---
+++ src/data/songs.ts
@@ -0,0 +1,32 @@
+import coverImage from "../assets/images/cover.jpg";
+import coverImage2 from "../assets/images/cover2.jpg";
+import coverImage3 from "../assets/images/cover3.jpg";
+import coverImage4 from "../assets/images/cover4.jpg";
+import coverImage5 from "../assets/images/cover5.jpg";
+import coverImage6 from "../assets/images/cover6.jpg";
+import coverImage7 from "../assets/images/musicCover.jpg";
+import coverImage8 from "../assets/images/cover8.jpg";
+import coverImage9 from "../assets/images/cover9.jpg";
+import coverImage10 from "../assets/images/cover10.jpg";
+import coverImage11 from "../assets/images/cover11.jpg";
+import coverImage12 from "../assets/images/cover12.jpg";
+import coverImage13 from "../assets/images/cover13.jpg";
+
+export const firstRow = [
+	{ title: "Hanuman Chalisa", singer: "Shankar Mahadevan", cover: coverImage },
+	{ title: "Raja ji Rusal Bani ka ", singer: "Pawan singh", cover: coverImage2 },
+	{ title: "Apan Raja ji", singer: "Pawan singh", cover: coverImage3 },
+	{ title: "Babuan", singer: "Pawan singh", cover: coverImage5 },
+	{ title: "Supreme", singer: "Subh", cover: coverImage6 },
+	{ title: "No Love", singer: "Subh", cover: coverImage7 },
+	{ title: "Pawan Singh Hits", singer: "Pawan singh", cover: coverImage4 },
+];
+export const secondRow = [
+	{ title: "SEE MY HYPE", singer: "Roop Bhullar and Wazir Patar", cover: coverImage9 },
+	{ title: "Kalkatiya Raja", singer: "Pawan singh", cover: coverImage8 },
+	{ title: "Dil Na Bikau Hamaar", singer: "Swati Mishra", cover: coverImage10 },
+	{ title: "lemonade", singer: "Diljit Dosanjh", cover: coverImage11 },
+	{ title: "Trust Me", singer: "The Boys", cover: coverImage6 },
+	{ title: "Born To shine", singer: "Diljit Dosanjh", cover: coverImage13 },
+	{ title: "Chaska", singer: "yo you Honey Singh", cover: coverImage12 },
+];
