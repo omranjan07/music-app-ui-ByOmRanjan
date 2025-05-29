5657dcaaa6657cd7bddae15d2f4d20e25fef854d
om ranjan <88664778+omranjan07@users.noreply.github.com>
2025-05-29 12:46:30 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Replace 'your-repo-name' with the actual repo name
 export default defineConfig({
-  base: 'music-app-ui-ByOmRanjan',
+  base: '/music-app-ui-ByOmRanjan/',
   plugins: [react()],
 })
-
