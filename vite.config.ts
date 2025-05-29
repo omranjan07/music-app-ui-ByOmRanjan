36cd500c676f5616f6f2b928317102c93b231bdc
Om <omranjankvrajgir@gmail.com>
2025-05-29 13:19:19 +0530

    Fix script src path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-// vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-=======
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
->>>>>>> ef25622 (Configure GitHub Pages deployment)
 
 export default defineConfig({
   base: '/music-app-ui-ByOmRanjan/',
