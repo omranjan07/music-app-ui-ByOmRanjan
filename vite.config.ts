1e7aedc40eb92280dde1797e78aed5117595bc4b
Om <omranjankvrajgir@gmail.com>
2025-05-29 13:00:44 +0530

    Save local changes before rebase

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
+<<<<<<< HEAD
 // vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+=======
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+>>>>>>> ef25622 (Configure GitHub Pages deployment)
 
 export default defineConfig({
   base: '/music-app-ui-ByOmRanjan/',
   plugins: [react()],
-})
+});
